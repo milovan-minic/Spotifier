1e2bafa3a78c6f335b1bc0d9d902d07251dc5b3d
Milovan Minic <milovan.minic@gmail.com>
2016-11-24 21:05:05 +0100

    Created table view controller and cell controller

---
+++ Spotifier/TrackCell.swift
@@ -0,0 +1,24 @@
+//
+//  TrackCell.swift
+//  Spotifier
+//
+//  Created by iosakademija on 11/24/16.
+//  Copyright © 2016 iOS akademija. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
