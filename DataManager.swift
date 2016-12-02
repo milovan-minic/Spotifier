8a2fb6875d1ee94661a1123d4c864f1da260cd0c
Milovan Minic <milovan.minic@gmail.com>
2016-12-02 20:45:54 +0100

    Initial parsing of the result, based on requested path Extract the actual data we need, in this case array of Tracks

--- DataManager.swift
+++ DataManager.swift
@@ -24,6 +24,17 @@ final class DataManager {
             
             if let _ = error {return}
             
+            guard let json = json else {return}
+            
+            switch type {
+            case .track:
+                guard let trackResult = json["tracks"] as? Spotify.JSON else {return}
+                guard let items = trackResult["items"] as? [Spotify.JSON] else {return}
+                
+            default:
+                break
+            }
+            
             // Process JSON or errors
             
         }
