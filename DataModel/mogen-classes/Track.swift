da4dff49e6adc12d495cd7befd60e23e5f9dd07d
Milovan Minic <milovan.minic@gmail.com>
2016-12-10 11:11:25 +0100

    Copied extension from old Track class

--- DataModel/mogen-classes/Track.swift
+++ DataModel/mogen-classes/Track.swift
@@ -15,3 +15,38 @@ required public init?(managedObjectContext moc: NSManagedObjectContext) {
     super.init(entity: entity, insertInto: moc)
     }
 }
+
+extension Track {
+    
+    convenience init?(json: [String: Any], in context: NSManagedObjectContext) {
+        self.init(context: context)
+        
+        do {
+            guard let trackId = json["id"] as? String else { throw DataImportError.typeMismatch(expected: String.self, actual: type(of: json["id"]), key: "id") }
+            guard let name = json["name"] as? String else { throw DataImportError.typeMismatch(expected: String.self, actual: type(of: json["name"]), key: "name") }
+            
+            self.trackId = trackId
+            self.name = name
+            
+            if let discNum = json["disc_number"] as? Int16 {
+                self.discNumber = discNum
+            }
+            if let durationMilliseconds = json["duration_ms"] as? Int64 {
+                self.durationMilliseconds = durationMilliseconds
+            }
+            
+        } catch(let error) {
+            switch error {
+            case is DataImportError:
+                print(error)
+            default:
+                print("some other error")
+            }
+            
+            //	if processing fails, then throw it out
+            context.delete(self)
+        }
+        
+    }
+}
+
